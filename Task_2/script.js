9d103836c03d3fba508e14a0279efaf58a53acff
Gandhi-Devloper <123054177+Gandhi-Devloper@users.noreply.github.com>
2023-07-12 16:15:40 +0530

    script.js

--- Task_2/script.js
+++ Task_2/script.js
@@ -1,13 +1,26 @@
-let celsius = document.getElementById("celsius");
-let fahrenheit = document.getElementById("fahrenheit");
+const celsiusElem = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
 
-function celToFar(){
-    let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
-    fahrenheit.value = parseFloat(output.toFixed(2));
-}
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusElem.innerHTML = "";
+});
 
-function farToCel(){
-    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
-    celsius.value = parseFloat( output.toFixed(2));
-    console.log(output);
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+
+if (tempType.value === "fahrenheit") {
+    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+  } else if (tempType.value === "kelvin") {
+    const KelvinToCelsius = inputValue - 273.15;
+    celsiusElem.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
+  }
 }
